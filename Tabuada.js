1983dd8622165e4caf2f3df863df9a57d371ea54
WendelSReis <90618369+WendelSReis@users.noreply.github.com>
2021-11-11 18:17:24 -0300

    Create Tabuada.js

---
+++ Tabuada.js
@@ -0,0 +1,13 @@
+<script>
+	var T = 5;
+	document.write("5 vezes 1 é " + T * 1+ "<br>");
+    document.write("5 vezes 2 é " + T * 2+ "<br>");
+    document.write("5 vezes 3 é " + T * 3+ "<br>");
+    document.write("5 vezes 4 é " + T * 4+ "<br>");
+    document.write("5 vezes 5 é " + T * 5+ "<br>");
+    document.write("5 vezes 6 é " + T * 6+ "<br>");
+    document.write("5 vezes 7 é " + T * 7+ "<br>");
+    document.write("5 vezes 8 é " + T * 8+ "<br>");
+    document.write("5 vezes 9 é " + T * 9+ "<br>");
+    document.write("5 vezes 10 é " + T * 10+ "<br>");
+</script>
